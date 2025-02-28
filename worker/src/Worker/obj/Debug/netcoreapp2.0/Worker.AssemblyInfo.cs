0f8738fa825cfd6a7dd8983c3e25503e139e4428
Dmitry Zhuravlev <zhdm.dev@gmail.com>
2025-02-28 10:52:34 -0500

    create fork

--- worker/src/Worker/obj/Debug/netcoreapp2.0/Worker.AssemblyInfo.cs
+++ worker/src/Worker/obj/Debug/netcoreapp2.0/Worker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Worker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea62c4257b868efafefc3dec0cdb3ccf3587782d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266cd35e47dd66f33ac69442a1fbaa519168a009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Worker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Worker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
